074647e8d9264bdb8123f6b8c498ac586008f345
Harsh Patel <harshpatel15505@gmail.com>
2025-08-04 23:21:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='flex justify-between bg-amber-300 '>
+      <h1 className='text-2xl underline '>
+        harsh resume analyser
+      </h1>
+      <span>
+        harshu
+      </span>
+
+    </div>
+  
+  )
+}
+
+export default App
